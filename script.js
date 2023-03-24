e9e76dfbb19b5a32ddec8c167f36b5dc27e827f1
Jose Barrios <devmusbar@gmail.com>
2023-03-23 20:27:03 -0500

    Portafolio

---
+++ script.js
@@ -0,0 +1,24 @@
+var typed = new Typed(".typing",{
+    strings: ["FullStack Developer","Student"], 
+    typeSpeed:100,
+    backSpeed: 100, 
+    backDelay:1000, 
+    loop: true
+});
+
+var typed2 = new Typed(".typing-2",{
+    strings: ["FullStack Developer Student"], 
+    typeSpeed:100,
+    backSpeed: 100, 
+    backDelay:1000, 
+    loop: true
+});
+
+var typed3 = new Typed(".typing-3",{
+    strings: ["Skills"], 
+    typeSpeed:100,
+    backSpeed: 100, 
+    backDelay:1000, 
+    loop: true
+});
+
